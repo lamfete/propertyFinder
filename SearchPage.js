3ebd1ab3ff81daa2433862329f30cf2a672d0629
lamfete <lamfete@yahoo.com>
2018-08-21 16:54:24 +0700

    handling assets

--- SearchPage.js
+++ SearchPage.js
@@ -40,6 +40,8 @@ export default class SearchPage extends Component<Props> {
                         title='Go'
                     />
                 </View>
+
+                <Image source={require('./Resources/house.png')} style={styles.image}/>
             </View>
         );
     }
@@ -72,5 +74,9 @@ const styles = StyleSheet.create({
         borderColor: '#48BBEC',
         borderRadius: 8,
         color: '#48BBEC',
+    },
+    image: {
+        width: 217,
+        height: 138,
     },
 });
