cfc0aaf040b12b2010aa7ab05cfa4086c87e3ec6
lamfete <lamfete@yahoo.com>
2018-08-23 13:06:58 +0700

    initiating a search

--- SearchPage.js
+++ SearchPage.js
@@ -11,6 +11,24 @@ import {
     Image,
 } from 'react-native';
 
+function urlForQueryAndPage(key, value, pageNumber) {
+    const data = {
+        country: 'uk',
+        pretty: '1',
+        encoding: 'json',
+        listing_type: 'buy',
+        action: 'search_listings',
+        page: pageNumber,
+    };
+    data[key] = value;
+
+    const querystring = Object.keys(data)
+        .map(key => key + '=' + encodeURIComponent(data[key]))
+        .join('&');
+
+    return 'https://api.nestoria.co.uk/api?' + querystring;
+}
+
 type Props = {};
 export default class SearchPage extends Component<Props> {
     static navigationOptions = {
@@ -20,18 +38,30 @@ export default class SearchPage extends Component<Props> {
     constructor(props) {
         super(props);
         this.state = {
-            searchString: 'london'
+            searchString: 'london',
+            isLoading: false,
         };
     }
 
     _onSearchTextChanged = (event) => {
-        console.log('_onSearchTextChanged');
+        // console.log('_onSearchTextChanged');
         this.setState({ searchString: event.nativeEvent.text });
-        console.log('Current: ' + this.state.searchString + ', Next: ' + event.nativeEvent.text);
+        // console.log('Current: ' + this.state.searchString + ', Next: ' + event.nativeEvent.text);
     };
 
+    _executeQuery = (query) => {
+        console.log(query);
+        this.setState({ isLoading: true });
+    }
+
+    _onSearchPressed = () => {
+        const query = urlForQueryAndPage('place_name', this.state.searchString, 1);
+        this._executeQuery(query);
+    }
+
     render() {
-        console.log('SearchPage.render')
+        // console.log('SearchPage.render');
+        const spinner = this.state.isLoading ? <ActivityIndicator size='large'/> : null;
         return(
             <View style={styles.container}>
                 <Text style={styles.description}>
@@ -51,13 +81,14 @@ export default class SearchPage extends Component<Props> {
                     />
 
                     <Button
-                        onPress={() => {}}
+                        onPress={this._onSearchPressed}
                         color='#48BBEC'
                         title='Go'
                     />
                 </View>
 
                 <Image source={require('./Resources/house.png')} style={styles.image}/>
+                {spinner}
             </View>
         );
     }
